23785e5ed411eec780cbcd429f6cb2f42d25bf6c
Zaravira <xladyhawkx@gmail.com>
2025-08-11 12:34:35 +0100

    improved docking controls to dock at closest station

--- LHMC_SVNoBuildingRestrictions/LHMC_SVNoBuildingRestrictions.cs
+++ LHMC_SVNoBuildingRestrictions/LHMC_SVNoBuildingRestrictions.cs
@@ -1,5 +1,6 @@
 ﻿using BepInEx;
 using HarmonyLib;
+using System.Linq;
 using UnityEngine;
 
 namespace LHMC_SVNoBuildingRestrictions
@@ -9,7 +10,7 @@ namespace LHMC_SVNoBuildingRestrictions
     {
         public const string pluginGuid = "LHMC_SVNoBuildingRestrictions";
         public const string pluginName = "LHMC_SVNoBuildingRestrictions";
-        public const string pluginVersion = "0.0.1";
+        public const string pluginVersion = "0.0.2";
 
         public void Awake()
         {
@@ -44,6 +45,82 @@ namespace LHMC_SVNoBuildingRestrictions
             if (other.gameObject.transform.name == "DockingControl")
                 return false;
             return true;
+        }
+
+        [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.CallOpenInventory))]
+        [HarmonyPrefix]
+        private static bool PCOpenInv_Pre(PlayerControl __instance)
+        {
+            StationCapture.allowClosing = true;
+            int num = MenuControl.ClosePanels(forceCloseAll: false);
+            if (num != 1 && num != 2)
+            {
+                if (__instance.inStationRange)
+                {
+                    // Get nearest station whose docking area is colliding with player
+                    Station station = null;
+                    float distCur = 99999;
+                    CapsuleCollider[] playerShipColliders = __instance.GetSpaceShip.GetComponentsInChildren<CapsuleCollider>();
+
+                    foreach (CapsuleCollider col in playerShipColliders)
+                    {
+                        Vector3 direction = new Vector3 { [col.direction] = 1 };
+                        float offset = col.height / 2 - col.radius;
+                        Vector3 localPoint0 = col.center - direction * offset;
+                        Vector3 localPoint1 = col.center + direction * offset;
+                        Vector3 r = __instance.GetSpaceShip.transform.TransformVector(col.radius, col.radius, col.radius);
+                        float radius = Enumerable.Range(0, 3).Select(xyz => xyz == col.direction ? 0 : r[xyz])
+                            .Select(Mathf.Abs).Max();
+
+                        Collider[] otherColliders = Physics.OverlapCapsule(
+                            __instance.GetSpaceShip.transform.TransformPoint(localPoint0),
+                            __instance.GetSpaceShip.transform.TransformPoint(localPoint1),
+                            radius, 2147483647, QueryTriggerInteraction.Collide);
+
+                        for (int i = 0; i < otherColliders.Length; i++)
+                        {
+                            Collider otherCol = otherColliders[i];
+                            if (otherCol.name == "DockingControl")
+                            {
+                                AIStationControl aisc = otherCol.gameObject.transform.parent.GetComponentInChildren<AIStationControl>();
+                                if (aisc != null)
+                                {
+                                    if (station != null &&
+                                        (aisc.station.HasDocking(false) && !DockingUI.inst.dockingPanel.activeSelf) &&
+                                        (GameData.data.gameMode == 1 || aisc.station.IsPlayerFriendly || aisc.station.Disabled) &&
+                                        (aisc == null || (aisc.ss != null && !aisc.ss.ffSys.TargetIsEnemy(__instance.GetSpaceShip.ffSys))))
+                                    {
+                                        float distNew = Vector3.Distance(__instance.GetSpaceShip.transform.position, aisc.gameObject.transform.position);
+                                        if (distNew < distCur)
+                                        {
+                                            station = aisc.station;
+                                            distCur = distNew;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        station = aisc.station;
+                                        distCur = Vector3.Distance(__instance.GetSpaceShip.transform.position, aisc.gameObject.transform.position);
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    if (station != null)
+                        DockingUI.inst.station = station;
+
+                    __instance.dockingUI.StartDockingStation(0);
+                    FleetControl.instance.DockFleet(forceDock: false);
+                }
+                else
+                {
+                    ShipInfo si = (ShipInfo)AccessTools.Field(typeof(PlayerControl), "shipInfo").GetValue(__instance);
+                    AccessTools.Method(typeof(ShipInfo), nameof(ShipInfo.OpenClose)).Invoke(si, null);
+                }
+            }
+
+            return false;
         }
     }
 }
